4e4b4656f91666272527adfbef9b9fbd0972c643
Ernst Reutergårdh <kedre@kth.se>
2020-11-19 18:55:10 +0100

    All REST-controllers have a basic structure

--- src/main/java/Backend/demo/Controllers/UserController.java
+++ src/main/java/Backend/demo/Controllers/UserController.java
@@ -1,11 +1,23 @@
 
 package Backend.demo.Controllers;
 
+import Backend.demo.bo.User;
 import Backend.demo.handler.UserHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    @PostMapping("newUser")
+    public void addUser(@RequestBody User newUser)
+    {
+        UserHandler.createUser(newUser.getName(), newUser.getPass());
+    }
+    @GetMapping("/login")
+    public String login(@RequestBody User currentUser)
+    {
+        return UserHandler.login(currentUser.getName(), currentUser.getPass());
+    }
 }
