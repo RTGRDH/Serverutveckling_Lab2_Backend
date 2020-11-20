18a7d4317bf583fe96d57f8e092a8091d3092d85
Ernst Reutergårdh <kedre@kth.se>
2020-11-20 15:29:01 +0100

    Loginmethod in UserHandler returns a bool

--- src/main/java/Backend/demo/handler/UserHandler.java
+++ src/main/java/Backend/demo/handler/UserHandler.java
@@ -8,13 +8,13 @@ import java.util.Collection;
 import java.util.Iterator;
 public class UserHandler {
 
-    public static String login(String user, String pass) {
+    public static Boolean login(String user, String pass) {
         if(User.login(user, pass)){
             //HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
             //session.setAttribute("currentUser", user);
-            return "Index";
+            return true;
         }
-        return "login";
+        return false;
     }
     public static boolean createUser(String userN, String passW){
         User.createUser(userN, passW);
